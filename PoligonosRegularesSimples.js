8753fc07eab8ddfd661f3c31c11e7f8ad8c8967c
Elton Freitas <eltons.freitas01@gmail.com>
2021-12-31 20:28:24 -0300

    commit de excercícios Programando com JS

---
+++ PoligonosRegularesSimples.js
@@ -0,0 +1,26 @@
+/*
+### Desafio
+Na geometria Euclidiana, um polígono regular é um polígono em que todos os ângulos são iguais e todos os lados tem o mesmo comprimento. Um polígono simples é aquele cujos segmentos de reta não se interceptam. Abaixo pode-se ver vários mosaicos feitos por polígonos regulares.
+
+Você deve escrever um programa que, dados o número e o comprimento dos lados de um polígono regular, mostre seu perímetro.
+
+
+### Entrada
+A entrada tem dois inteiros positivos: N e L, que são, respectivamente, 
+o número de lados e o comprimento de cada lado de um polígono regular (3 ≤ N ≤ 1000000 and 1 ≤ L ≤ 4000).
+
+### Saída
+A saída é o perímetro P do polígono regular em uma única linha.
+
+*/
+
+let lines = gets().split("\n");
+let line = lines.shift().split(' ');
+
+//continue o seu código aqui
+
+var arrInt = line.map(function(item) {
+    return parseInt(item, 10);
+});
+
+console.log(arrInt[0] * arrInt[1]); 
